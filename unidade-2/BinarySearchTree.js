36bf3ca08ec6c585881b2d4832ff54844aa26736
Gabrielly Freire <gabylopesfreire@gmail.com>
2024-12-08 17:24:28 -0300

    feat: add AVLTree class and add methords new in BinarySearchTree

--- unidade-2/BinarySearchTree.js
+++ unidade-2/BinarySearchTree.js
@@ -10,27 +10,140 @@ export default class BinarySearchTree {
         this.root = null;
     }
 
-    Insert(key) {
-        if (this.root == null) {
-            this.root = new Node(key);
-        } else {
-            this.InsertNode(this.root, key);
+    /**
+     * Percurso em ordem na árvore binária de busca
+     * @param {*} callback 
+     */
+    inOrderTraverse(callback) {
+        this.inOrderTraverseNode(this.root, callback);
+    }
+
+    inOrderTraverseNode(node, callback) {
+        if (node != null) {
+            this.inOrderTraverseNode(node.left, callback);
+            callback(node.key);
+            this.inOrderTraverseNode(node.right, callback);
         }
     }
 
-    InsertNode(node, key) {
+    /**
+     * Percurso pre-ordem na árvore binária de busca
+     * @param {*} callback 
+     */
+    preOrderTraverse(callback) {
+        this.preOrderTraverseNode(this.root, callback);
+    }
+
+    preOrderTraverseNode(node, callback) {
+        if (node != null) {
+            callback(node.key);
+            this.preOrderTraverseNode(node.left, callback);
+            this.preOrderTraverseNode(node.right, callback);
+        }
+    }
+
+    /**
+     * Percurso pós-ordem na árvore binária de busca
+     * @param {*} callback 
+     */
+    postOrderTraverse(callback) {
+        this.postOrderTraverseNode(this.root, callback);
+    }
+
+    postOrderTraverseNode(node, callback) {
+        if (node != null) {
+            this.postOrderTraverseNode(node.left, callback);
+            this.postOrderTraverseNode(node.right, callback);
+            callback(node.key);
+        }
+    }
+
+    /**
+     * Busca por um elemento na árvore binária de busca
+     */
+    search(key) {
+        return this.searchNode(this.root, key);
+    }
+
+    searchNode(node, key) {
+        if (node == null) {
+            return false;
+        }
+
+        if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+            return this.searchNode(node.left, key);
+        } else if (this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
+            return this.searchNode(node.right, key);
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Inserção de um elemento na árvore binária de busca
+     * @param {*} key 
+     */
+    insert(key) {
+        if (this.root == null) {
+            this.root = new Node(key);
+        } else {
+            this.insertNode(this.root, key);
+        }
+    }
+
+    insertNode(node, key) {
         if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
             if (node.left == null) {
                 node.left = new Node(key);
             } else {
-                this.InsertNode(node.left, key);
+                this.insertNode(node.left, key);
             }
         } else {
             if (node.right == null) {
                 node.right = new Node(key);
             } else {
-                this.InsertNode(node.right, key);
+                this.insertNode(node.right, key);
             }
+        }
+    }
+
+    remove(key) {
+        this.root = this.removeNode(this.root, key);
+    }
+
+    removeNode(node, key) {
+        if(node == null) {
+            return null;
+        }
+
+        if(this.compareFn(key, node.key) === Compare.LESS_THAN) {
+            node.left = this.removeNode(node.left, key);
+            return node;
+        } else if(this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
+            node.right = this.removeNode(node.right, key);
+            return node;
+        } else {
+
+            // Caso 1 - Folha
+            if(node.left == null && node.right == null) {
+                node = null;
+                return node;
+            }
+
+            // Caso 2 - Nó com um filho
+            if(node.left == null) {
+                node = node.right;
+                return node;
+            } else if(node.right == null) {
+                node = node.left;
+                return node;
+            }
+
+            // Caso 3 - Nó com dois
+            const aux = this.minNode(node.right);
+            node.key = aux.key;
+            node.right = this.removeNode(node.right, aux.key);
+            return node;
         }
     }
 }
